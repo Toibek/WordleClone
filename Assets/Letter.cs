853ad2955e9b31fd5e80d25bada0c20ee02d093d
Tobias Ek <Bodentobbe@hotmail.com>
2022-11-07 13:54:48 +0100

    Words, but not effectivley

--- Assets/Letter.cs
+++ Assets/Letter.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Letter : MonoBehaviour
 {
-    [Tooltip("0: Default, 1: WrongPlace, 2:Correct")]
-    [SerializeField] private Color[] colors;
+    [Tooltip("0: Default, 1:Wrong , 2: WrongPlace, 3:Correct")]
+    [SerializeField] private Color[] _colors;
+    [SerializeField] private Color[] _textColors;
+    [SerializeField] private letterState _currentState;
+    [SerializeField] private bool _colorTest;
 
-    public enum letterState { Default, WrongPlace, Correct }
+    Image _background;
+    TMP_Text _text;
+    private void Start()
+    {
+        _background = GetComponent<Image>();
+        _text = GetComponentInChildren<TMP_Text>();
+
+        _background.color = _colors[(int)_currentState];
+        _text.color = _textColors[(int)_currentState];
+    }
+    private void Update()
+    {
+        if (_colorTest)
+        {
+            _colorTest = false;
+            _background.color = _colors[(int)_currentState];
+            _text.color = _textColors[(int)_currentState];
+        }
+    }
+
+    public enum letterState { Default = 0, Wrong = 1, WrongPlace = 2, Correct = 3 }
 }
