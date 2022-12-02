d108ab765fe949d0a952cb267db86b8f7d278b92
Tobias Ek <Bodentobbe@hotmail.com>
2022-12-02 17:00:12 +0100

    Reading from file, submit broken in build

--- Assets/Scripts/Words.cs
+++ Assets/Scripts/Words.cs
@@ -7,7 +7,7 @@ public static class Words
     static HashSet<string> words;
     public static void LoadWords()
     {
-        string path = Application.persistentDataPath + "/words.txt";
+        string path = Application.dataPath + "/Resources/words.txt";
         Debug.Log(path);
         words = new(File.ReadAllLines(path));
     }
@@ -20,6 +20,5 @@ public static class Words
     {
         if (words == null || words.Count == 0) LoadWords();
         return words.ElementAt(Random.Range(0, words.Count));
-
     }
 }
